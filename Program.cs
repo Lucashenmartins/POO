3e52b84eba3827f8fed48745496447f2adc0fcb3
Lucashenmartins <149275076+Lucashenmartins@users.noreply.github.com>
2025-09-09 00:32:23 +0000

    final exercicios aula 5

--- Program.cs
+++ Program.cs
@@ -1,6 +1,171 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 Console.WriteLine("Hello, World!");
 
 Televisao tv = new Televisao(55f);
 
 Console.WriteLine($"A TV tem o tamanho {tv.Tamanho}");
+
+tv.MudarCanalInput();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.BotaoMudoLiga();
+
+tv.BotaoMudoDesliga();
+
+tv.MudarUmCanalAbaixo();
+Console.WriteLine($"Canal {tv.Canal}");
+tv.MudarUmCanalAcima();
+
+Console.WriteLine("Aqui começamos os testes");
+
+Console.WriteLine("Primeiro, Volume Maximo");
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+Console.WriteLine("Volume Minimo");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+Console.WriteLine("Botão liga desliga");
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.BotaoMudoLiga();
+
+tv.BotaoMudoDesliga();
+
+Console.WriteLine($"Volume {tv.Volume}");
+
+Console.WriteLine("Canais cima e baixo");
+
+tv.MudarUmCanalAbaixo();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAbaixo();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAbaixo();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAbaixo();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAbaixo();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAcima();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAcima();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAcima();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAcima();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAcima();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAcima();
+Console.WriteLine($"Canal {tv.Canal}");
+
+Console.WriteLine("Canais maximos ou minimos");
+Console.WriteLine("Colocando o canal no 3 e descendo");
+tv.Canal = 3;
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAbaixo();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAbaixo();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAbaixo();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAbaixo();
+Console.WriteLine($"Canal {tv.Canal}");
+
+Console.WriteLine("Colocando o canal no 517 e subindo");
+tv.Canal = 517;
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAcima();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAcima();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAcima();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.MudarUmCanalAcima();
+Console.WriteLine($"Canal {tv.Canal}");
--- Televisao.cs
+++ Televisao.cs
@@ -1,9 +1,20 @@
+using System.Runtime.InteropServices;
+
 public class Televisao
 {
-
+    public const int VOL_MAX = 10;
+    public const int VOL_MIN = 0;
+    public const int CANAL_MIN = 0;
+    public const int CANAL_MAX = 520;
+    public int ULTIMO_CANAL;
+    public int ULTIMO_VOLUME;
+    public int MUDAR_CANAL;
+    public int VOLUME_PADRAO = 5;
     public Televisao(float tamanho)
     {
         Tamanho = tamanho;
+
+        Volume = VOLUME_PADRAO;
 
     }
 
@@ -15,7 +26,83 @@ public class Televisao
 
     public void AumentarVolume()
     {
-        Volume = Volume++
+        if (Volume < VOL_MAX)
+        {
+            Volume++;
+        }
+        else
+        {
+            Volume = VOL_MAX;
+            Console.WriteLine("Volume ja esta no maximo");
+        }
     }
-    
+    public void DiminuirVolume()
+    {
+        if (Volume > VOL_MIN)
+        {
+            Volume--;
+        }
+        else
+        {
+            Volume = VOL_MIN;
+            Console.WriteLine("Volume ja esta no minimo");
+        }
+    }
+    public void BotaoMudoLiga()
+    {
+        if (Volume > VOL_MIN)
+        {
+            ULTIMO_VOLUME = Volume;
+            Volume = VOL_MIN;
+            Console.WriteLine("TV em Modo Mudo");
+        }
+    }
+    public void BotaoMudoDesliga()
+    {
+        if (Volume == VOL_MIN)
+        {
+            Volume = ULTIMO_VOLUME;
+            Console.WriteLine("TV saiu do Modo Mudo");
+        }
+    }
+    public void MudarUmCanalAcima()
+    {
+        if (Canal < CANAL_MAX)
+        {
+            Canal++;
+            ULTIMO_CANAL = Canal;
+        }
+        else
+        {
+            Canal = CANAL_MIN;
+        }
+    }
+    public void MudarUmCanalAbaixo()
+    {
+        if (Canal > CANAL_MIN)
+        {
+
+            Canal--;
+            ULTIMO_CANAL = Canal;
+        }
+        else
+        {
+            Canal = CANAL_MAX;
+        }
+    }
+    public void MudarCanalInput()
+    {
+        Console.WriteLine("Qual canal mudar:");
+        MUDAR_CANAL = Convert.ToInt32(Console.ReadLine());
+
+        if (MUDAR_CANAL < CANAL_MIN || MUDAR_CANAL > CANAL_MAX)
+        {
+            Console.WriteLine("Canal Invalido");
+        }
+        else
+        {
+            Canal = MUDAR_CANAL;
+        }
+   
+    }
 }
